55132d8eb7e67ee26e94230762c1c58b69c184e6
urk3 <urk3@txstate.edu>
2025-01-31 14:55:57 -0600

    tweaking bonus words to get seperation of files correct, then will write something to group them togethor based on similarity

--- sorter.py
+++ sorter.py
@@ -4,18 +4,35 @@ import math
 import nltk
 import numpy as np # for dop prodoct
 from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+from nltk.corpus import wordnet
 
 # nltk.download('wordnet')
-
+# nltk.download('averaged_perceptron_tagger_eng')
+        
 stopWords = { 'a', 'an', 'the', 'and', 'or', 'but', 'to', 'of', 'for', 'in', 'on', 'at', 'by', 'with', 'from', 'your', 'we', 'our', 'is', 'that', 'like', 'you', 
              'move', 'have', 'this', 'can', 'still', 'whom', 'other', 'about', 'wish', 'make', 'will', 'eye', 'after', 'when', 'do', 'out', 'dear' }
 ## nltk removes stop words i like, so i use my own list
+    
+def getWordBase(tag): # added this because the calculates were off and i thught lemmy alr did this
+    return {
+        'N' : wordnet.NOUN,
+        'V' : wordnet.VERB,
+        'J' : wordnet.ADJ,
+        'R' : wordnet.ADV
+    }.get(tag[0], wordnet.NOUN) # i think most are nouns maybe
+    
 def processText(rawText):
     rawText = re.sub(r'[^a-zA-Z\'\s]', '', rawText) # remove stuff like special characters
     rawText = re.sub(r'http\S+', '', rawText) # remove url
-    tokens = re.findall(r"\b(?:[a-z]+(?:['’-]?[a-z]+)*)+\b", rawText.lower()) 
+    tokens = pos_tag(re.findall(r"\b(?:[a-z]+(?:['’-]?[a-z]+)*)+\b", rawText.lower())) 
     lemmy = WordNetLemmatizer()
-    return [lemmy.lemmatize(token) for token in tokens if token not in stopWords and len(token) > 2]
+    lemmatized = []
+    for word, tag in tokens:
+        pos = getWordBase(tag)
+        lemma = lemmy.lemmatize(word, pos=pos)
+        lemmatized.append(lemma)
+    return [token for token in lemmatized if token not in stopWords and len(token) > 2]
     
 def normalize(vector):
     normalization = np.linalg.norm(vector)
@@ -27,10 +44,9 @@ def cosineSim(a, b):
 uniqueVocabulary = {} # for idf vecotrization, keys are the unique words, which are string, and the vlaues are an integer which represents number of occurances across all documents
 documentTF = []
 path = r"/mnt/c/Users/Schmoopy/Documents/EmailSorter/emails" #ubunto not cmd
-
-# boostWords = {"not": 1.5, "interview": 1.5}
+boostWords = {"invite": 5, "not": 5}
 # boostWords = {}
-boostWords = {"not": 3}
+# boostWords = {"not": 3}
 
 for filename in os.listdir(path): # need to calculate tf, frequency in an email, and idf, frequency across all emails
     file_path = os.path.join(path, filename)
@@ -53,6 +69,7 @@ for filename in os.listdir(path): # need to calculate tf, frequency in an email,
             else:
                 uniqueVocabulary[word] = 1  
 
+# print(uniqueVocabulary) #testing
 # the above two nested for loops can be combined, just not yet
 uniqueVocabularyIDF = {word: math.log(len(os.listdir(path)) / (df + 1)) for word, df in uniqueVocabulary.items()}
 
@@ -72,4 +89,4 @@ normalized_vectors = [normalize(v) for v in tfidfVector] # each index is a email
 for i in range(len(normalized_vectors)):
     for j in range(i + 1, len(normalized_vectors)):  
         similarity = cosineSim(normalized_vectors[i], normalized_vectors[j])
-        print(f"Similarity between email {i + 1} and email {j + 1}: {similarity:.8f}")
+        print(f"Similarity between email {i + 1} and email {j + 1}: {100 * similarity:.8f}")
