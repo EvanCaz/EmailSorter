4481fa0c0802d572688e369f5da6da598dd87d9f
urk3 <urk3@txstate.edu>
2025-01-31 14:26:36 -0600

    add boost words as the result i was expected was not what I wanted

--- sorter.py
+++ sorter.py
@@ -2,22 +2,36 @@ import re
 import os
 import math
 import nltk
+import numpy as np # for dop prodoct
 from nltk.stem import WordNetLemmatizer
 
 # nltk.download('wordnet')
 
-stopWords = { 'a', 'an', 'the', 'and', 'or', 'but', 'to', 'of', 'for', 'in', 'on', 'at', 'by', 'with', 'from', 'your', 'we', 'our', 'is', 'that', 'like', 'you', 'move', 'have', 'this', 'can', 'still', 'whom', 'other', 'about', 'wish', 'make', 'will', 'eye', 'after', 'when' }
+stopWords = { 'a', 'an', 'the', 'and', 'or', 'but', 'to', 'of', 'for', 'in', 'on', 'at', 'by', 'with', 'from', 'your', 'we', 'our', 'is', 'that', 'like', 'you', 
+             'move', 'have', 'this', 'can', 'still', 'whom', 'other', 'about', 'wish', 'make', 'will', 'eye', 'after', 'when', 'do', 'out', 'dear' }
 ## nltk removes stop words i like, so i use my own list
 def processText(rawText):
-    rawText = re.sub(r'[^a-zA-Z\'\s]', '', rawText)
-    tokens = re.findall(r"\b(?:[a-z]+(?:['’-][a-z]+)*)+\b", rawText.lower())
+    rawText = re.sub(r'[^a-zA-Z\'\s]', '', rawText) # remove stuff like special characters
+    rawText = re.sub(r'http\S+', '', rawText) # remove url
+    tokens = re.findall(r"\b(?:[a-z]+(?:['’-]?[a-z]+)*)+\b", rawText.lower()) 
     lemmy = WordNetLemmatizer()
     return [lemmy.lemmatize(token) for token in tokens if token not in stopWords and len(token) > 2]
     
+def normalize(vector):
+    normalization = np.linalg.norm(vector)
+    return vector / normalization if normalization != 0 else vector
+
+def cosineSim(a, b):
+    return np.dot(a, b) # love np
+
 uniqueVocabulary = {} # for idf vecotrization, keys are the unique words, which are string, and the vlaues are an integer which represents number of occurances across all documents
 documentTF = []
-    
 path = r"/mnt/c/Users/Schmoopy/Documents/EmailSorter/emails" #ubunto not cmd
+
+# boostWords = {"not": 1.5, "interview": 1.5}
+# boostWords = {}
+boostWords = {"not": 3}
+
 for filename in os.listdir(path): # need to calculate tf, frequency in an email, and idf, frequency across all emails
     file_path = os.path.join(path, filename)
     with open(file_path, "r") as file:
@@ -27,8 +41,10 @@ for filename in os.listdir(path): # need to calculate tf, frequency in an email,
     wordCounts = {} # dict for tf
     totalWord = len(text)
     for word in text:
-        wordCounts[word] = wordCounts.get(word, 0) + 1 # like that leetcode question 2225 i think
+        increment = boostWords.get(word, 1.0)
+        wordCounts[word] = wordCounts.get(word, 0) + increment # like that leetcode question 2225 i think
     tfCur = {word: count / totalWord for word, count in wordCounts.items()}
+    
     documentTF.append(tfCur) # each documents respective tf scores for each word
     
     for word in set(text): # so we only count each word once, right now with three docuemnts, the total number a word can appear is three and I had more before set but do not use this for tf as frequency matters
@@ -38,13 +54,22 @@ for filename in os.listdir(path): # need to calculate tf, frequency in an email,
                 uniqueVocabulary[word] = 1  
 
 # the above two nested for loops can be combined, just not yet
-vocab = sorted(uniqueVocabulary.keys())
 uniqueVocabularyIDF = {word: math.log(len(os.listdir(path)) / (df + 1)) for word, df in uniqueVocabulary.items()}
 
+# fcalculate a score for each word in the document with the idf scroe from dict and tf score of cur documenet, append all of them to a list, and append that list to list of all vectors 
+vocab = sorted(uniqueVocabulary.keys())
+tfidfVector = []
+for i in documentTF: 
+    vector = []
+    for word in vocab:
+        tfidfScore = i.get(word, 0.0) * uniqueVocabularyIDF.get(word, 0.0)
+        vector.append(tfidfScore)
+    tfidfVector.append(vector)
 
-# print(f"Unique words collected from all files: {documentTF}")
-# for i in range(len(documentTF)):
-#     print(documentTF[i])
-#     print()
-
-
+normalized_vectors = [normalize(v) for v in tfidfVector] # each index is a email
+# all vectors must be scalled by one because thats how cosine similarity works
+    
+for i in range(len(normalized_vectors)):
+    for j in range(i + 1, len(normalized_vectors)):  
+        similarity = cosineSim(normalized_vectors[i], normalized_vectors[j])
+        print(f"Similarity between email {i + 1} and email {j + 1}: {similarity:.8f}")
